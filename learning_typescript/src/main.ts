def637977c2980d8c720d64b86c749aded83396e
gek64 <10346089+gek64@users.noreply.github.com>
2022-05-25 18:40:24 +0800

    Add files via upload

---
+++ learning_typescript/src/main.ts
@@ -0,0 +1,4 @@
+import math = require("./math")
+
+let a: number = 1
+let b: number = 20
---
+++ learning_typescript/src/math.ts
@@ -0,0 +1,15 @@
+function add(n1: number, n2: number): number {
+    return n1 + n2
+}
+
+function sub(n1: number, n2: number): number {
+    return n1 - n2
+}
+
+function mul(n1: number, n2: number): number {
+    return n1 * n2
+}
+
+function div(n1: number, n2: number): number {
+    return n1 / n2
+}
