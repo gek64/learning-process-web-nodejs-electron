44774871daed1fb038c82e1c307db3896ec94d36
gek64 <10346089+gek64@users.noreply.github.com>
2022-05-31 00:04:30 +0800

    update

--- learning_typescript/src/main.ts
+++ learning_typescript/src/main.ts
@@ -1,4 +1,20 @@
-import math = require("./math")
+function add(n1: number, n2: number): number {
+    return n1 + n2
+}
+
+function sub(n1: number, n2: number): number {
+    return n1 - n2
+}
+
+function mul(n1: number, n2: number): number {
+    return n1 * n2
+}
+
+function div(n1: number, n2: number): number {
+    return n1 / n2
+}
 
 let a: number = 1
 let b: number = 20
+
+console.log(add(a,b))
